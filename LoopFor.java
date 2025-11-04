4f975b91fb40850ec742b00736688454363cc21b
Muad-syntax <mohammad.suaebummuad9@smk.belajar.id>
2025-11-04 14:46:38 +0700

    Loop (LEBIH BAIK BERHATI-HATI)

---
+++ LoopFor.java
@@ -0,0 +1,8 @@
+public class LoopFor {
+    public static void main(String[] args) {
+        System.out.println("--- Program Menampilkan angka 1 sampai 10 ---");
+        for (int i = 1; 1 <= 10; i++) {
+            System.out.println("Angka ke : " + i);
+        }
+    }
+}
