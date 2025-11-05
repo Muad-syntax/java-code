e01fba06d87a973a85e1064cae4f0396b5a9db6e
Muad-syntax <mohammad.suaebummuad9@smk.belajar.id>
2025-11-05 09:40:54 +0700

    memperbaiki eror

--- LoopFor.java
+++ LoopFor.java
@@ -1,7 +1,7 @@
 public class LoopFor {
     public static void main(String[] args) {
         System.out.println("--- Program Menampilkan angka 1 sampai 10 ---");
-        for (int i = 1; 1 <= 10; i++) {
+        for (int i = 1; i <= 2000; i++) {
             System.out.println("Angka ke : " + i);
         }
     }
