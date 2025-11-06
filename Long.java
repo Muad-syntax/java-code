f3dc603453da73a874ca927a750a4d40216f9668
Muad-syntax <mohammad.suaebummuad9@smk.belajar.id>
2025-11-06 07:23:13 +0700

    Long

---
+++ Long.java
@@ -0,0 +1,14 @@
+public class Long {
+    public static void main(String[] args){
+
+        long a = 10;
+        long b = 30;
+        long c = 10;
+        long d = a * b + c;
+
+        System.out.println();
+        System.out.println(d);
+        System.out.println();
+
+    }
+}
