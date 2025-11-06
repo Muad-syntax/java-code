98ac94a0f68f5d4e3cfa88feb80710fb6ddbdb62
Muad-syntax <mohammad.suaebummuad9@smk.belajar.id>
2025-11-06 07:26:18 +0700

    main

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args)
+    {
+        String Nama = "Muad";
+        int Umur = 21;
+        float tinggi = 160;
+            System.out.println();
+            System.out.println(Nama + ", umur kau " + Umur + " tahun ya..?");
+            System.out.println("tinggi " + tinggi + " cm " + "ya " + Nama);
+            System.out.println();
+    }
+}
